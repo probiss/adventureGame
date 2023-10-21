f826602cc896143213b2a30024b1992ab91c6e79
probiss <bulut.cihad@gmail.com>
2023-10-21 14:24:02 +0300

    select your battle location.

--- src/src/Game.java
+++ src/src/Game.java
@@ -18,6 +18,9 @@ public class Game {
             System.out.println();
                 System.out.println("1 - Güvenli Ev --> Burası sizin için güvenli bir ev, düşman yoktur !");
                 System.out.println("2 - Mağaza --> Silah veya Zırh satın alabilirsiniz !");
+                System.out.println("3 - Mağara --> Ödül <Yemek>, dikkatli ol karşına zombi çıkabilir !");
+                System.out.println("4 - Orman --> Ödül <Odun>, dikkatli ol karşına vampir çıkabilir !");
+                System.out.println("5 - Nehir --> Ödül <Water>, dikkatli ol karşına ayı çıkabilir !");
                 System.out.println("0 - Çıkış yap --> Oyunu sonlandır !");
                 System.out.println("Lütfen gitmek istediğiniz bölgeyi seçini : ");
                 int selectLoc = input.nextInt();
@@ -31,8 +34,17 @@ public class Game {
                     case 2:
                         location = new ToolStore(player);
                         break;
+                    case 3:
+                        location = new Cave(player);
+                        break;
+                    case 4:
+                        location = new Forest(player);
+                        break;
+                    case 5:
+                        location = new River(player);
+                        break;
                     default:
-                        location = new SafeHouse(player);
+                        System.out.println("Ltfen geçerli bir bölge seçiniz !");
                 }
 
                 if(location == null){
